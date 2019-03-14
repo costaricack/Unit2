9694b50a034b227259a4e5ab8a23b94aeb638f57
costaricack <47116675+costaricack@users.noreply.github.com>
2019-03-14 14:47:00 -0400

    Calculator Auto Layout

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Nicholas COOK on 3/13/19.
+//  Copyright © 2019 Nicholas COOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
